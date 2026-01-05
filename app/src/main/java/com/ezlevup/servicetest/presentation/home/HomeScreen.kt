d9fb5d32dbed3278d0fc43b9a9a453dc4a8ceaea
이영현 <hong.git@yahoo.com>
2026-01-05 15:49:20 +0900

    Initial commit

---
+++ app/src/main/java/com/ezlevup/servicetest/presentation/home/HomeScreen.kt
@@ -0,0 +1,13 @@
+package com.ezlevup.servicetest.presentation.home
+
+import androidx.compose.runtime.Composable
+import com.ezlevup.servicetest.presentation.component.Greeting
+
+@Composable
+fun HomeScreen(
+
+) {
+    Greeting(
+        name = "Android"
+    )
+}
